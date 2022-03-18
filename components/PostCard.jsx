bcf6cd4a3ee2939104bd304068db3046eb836c0e
Joshhortt <joshhortt@yahoo.com>
2022-03-18 22:14:59 +0000

    add alt tag to PostCard'.

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -21,7 +21,7 @@ const PostCard = ({ post }) => {
     </div> */}
 
     <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-      <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
+      <img src={post.featuredImage.url} alt="featured" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
     </div>
 
     <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
