0bc866c0db452d0929d3a2b5b6de1206891b3872
Joshhortt <joshhortt@yahoo.com>
2022-03-18 17:04:49 +0000

    add .env file with endpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState } from 'react';
 import { Layout } from '../components';
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
