74fb3dc54a6e5da99c36eec26b93739fe6337a96
Joshhortt <joshhortt@yahoo.com>
2022-03-21 12:44:44 +0000

    remove comments

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="bg-black text-white text-sm tracking-wide py-10 flex justify-center p-4">
-     
-        {/* <div className="flex justify-center"> */}
-
-            <div>&copy; {new Date().getFullYear()} | Creative Lightbox Media | JAMstack Web Development & Design</div>
-          {/* </div> */}
+     <div>&copy; {new Date().getFullYear()} | Creative Lightbox Media | JAMstack Web Development & Design</div> 
     </footer>
   );
 }
